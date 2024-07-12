3d23f401d503df793d60ac366ace31dc098974e0
brewchetta <chett.tiller@gmail.com>
2024-07-12 11:03:43 -0400

    add starter instructions, seeds

--- README.md
+++ README.md
@@ -4,6 +4,23 @@
 
 - Using the flask_restful library
 
+## Getting Started
+
+```bash
+pipenv install
+pipenv shell
+
+cd server
+
+flask db init
+flask db migrate -m "create sports table"
+flask db upgrade
+
+python seed.py
+
+python app.py
+```
+
 ## RESTful Routes
 
 !["restful routes chart"](assets/restful-routes.png)
--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from app import app
-from models import db # models go here
+from models import db, Sport
 from faker import Faker
 
 faker = Faker()
@@ -10,6 +10,13 @@ if __name__ == '__main__':
     with app.app_context():
         print("Seeding database...")
 
-        # write your seeds here!
+        Sport.query.delete()
+
+        s1 = Sport(name="Extreme Yodeling")
+        s2 = Sport(name="Ultimate Fridge Tossing")
+        s3 = Sport(name="Freestyle Trash Diving")
+
+        db.session.add_all([s1, s2, s3])
+        db.session.commit()
 
         print("Seeding complete!")
